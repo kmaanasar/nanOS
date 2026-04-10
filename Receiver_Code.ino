abe05f76887e01818c3f7116b717ea461341744e
Krishna Maanasa Ramadugu <125827551+kmaanasar@users.noreply.github.com>
2026-04-09 17:40:35 -0700

    Update Receiver_Code.ino

--- Receiver_Code.ino
+++ Receiver_Code.ino
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <SPI.h>
-#include <RH_RF69.h>
+#include <RH_RF95.h>
 
 // Define pin assignments for RFM95
 const int RFM95_CS  = D6;   // RFM95 Chip Select
@@ -10,10 +10,10 @@ const int RFM95_INT = D8;   // RFM95 Interrupt
 #define RF95_FREQ 900.0     // Must match transmitter frequency
 
 // Singleton instance of the radio driver
-RH_RF95 rf69(RFM95_CS, RFM95_INT);
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
 void setup() {
-  pinMode(LED, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
 
@@ -21,7 +21,7 @@ void setup() {
   Serial.begin(9600);
   delay(100);
 
-  Serial.println("NanoFloat reciver!");
+  Serial.println("NanoFloat receiver!");
 
   // Manual reset
   digitalWrite(RFM95_RST, LOW);
@@ -30,10 +30,10 @@ void setup() {
   delay(10);
 
   while (!rf95.init()) {
-    Serial.println("Nanofloat reciever init failed");
+    Serial.println("NanoFloat receiver init failed");
     while (1);
   }
-  Serial.println("Nanofloat reciever init OK!");
+  Serial.println("NanoFloat receiver init OK!");
 
   if (!rf95.setFrequency(RF95_FREQ)) {
     Serial.println("setFrequency failed");
@@ -45,18 +45,18 @@ void setup() {
 }
 
 void loop() {
-  if (rf95.available()){
+  if (rf95.available()) {
     uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
     uint8_t len = sizeof(buf);
 
-    if (rf95.recv(buf, &len))
-    {
-      digitalWrite(LED, HIGH);
+    if (rf95.recv(buf, &len)) {
+      digitalWrite(LED_BUILTIN, HIGH);
       RH_RF95::printBuffer("Received: ", buf, len);
       Serial.print("Got: ");
       Serial.println((char*)buf);
       Serial.print("RSSI: ");
       Serial.println(rf95.lastRssi(), DEC);
+      digitalWrite(LED_BUILTIN, LOW);
     } else {
       Serial.println("Receive failed");
     }
